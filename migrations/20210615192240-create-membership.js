c159f79ca1d912ee831c81ef0508d00cb98a07b6
Victoria Green <victoria.cousan@gmail.com>
2021-06-15 14:33:33 -0500

    database creation and model generation file

---
+++ migrations/20210615192240-create-membership.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('memberships', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userID: {
+        type: Sequelize.INTEGER
+      },
+      familyID: {
+        type: Sequelize.INTEGER
+      },
+      isApproved: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('memberships');
+  }
+};
