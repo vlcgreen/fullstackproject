19d55860b12d71f0cc34d4bc44daebdf24052f12
Jim Chamberlin <chamberlin.jim@gmail.com>
2021-06-16 15:24:07 -0400

    updated register.ejs

--- routes/register.js
+++ routes/register.js
@@ -25,16 +25,7 @@ const fileFilter = (req,file,cb) => {
 const upload = multer({storage:storage, fileFilter:fileFilter});
 
 router.get('/register',(req, res) => {
-    res.send(`
-    <form action="/register" method="post" enctype="multipart/form-data">
-    username
-    <input type="text" name="username" /><br>
-    password
-    <input type="text" name="password" /><br>
-    <input type="file" name="image" />
-    <input type="submit"/>
-    </form>
-    `)
+    res.render('register')
 })
 
 router.post('/register',upload.single('image'),async (req, res) => {
@@ -46,7 +37,7 @@ router.post('/register',upload.single('image'),async (req, res) => {
         } 
 
     //get info from header
-    let {username,password} = req.body;
+    let {username,password, email} = req.body;
     //hash our password using bcrypt
     let passwordEncrypted = bcrypt.hashSync(password,8);
 
@@ -56,7 +47,8 @@ router.post('/register',upload.single('image'),async (req, res) => {
         name:username,
         password:passwordEncrypted,
         photo:imagePath,
-        roleID:0
+        roleID:0,
+        email: email
     })
     res.redirect('/login');
     }
